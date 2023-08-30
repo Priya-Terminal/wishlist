5d0f0d4929ef5af035797f5e0c9819777529f48a
Krushi Raj Tula <ironcap.crypto@gmail.com>
2023-08-30 23:55:10 +0530

    fix: do not use layout in index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useContext } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import UserContext from "@/contexts/user";
-import Layout from "@/components/layout/Layout";
 
 const Home = () => {
   const [user] = useContext(UserContext);
@@ -15,35 +14,33 @@ const Home = () => {
   }, []);
 
   return (
-    <Layout>
-      <div className="min-h-screen bg-primary flex justify-center items-center">
-        <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-          <h1 className="text-4xl font-timesnewRoman mb-6 text-center text-primary text-black">
-            ShareWish
-          </h1>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-4xl font-timesnewRoman mb-6 text-center text-primary text-black">
+          ShareWish
+        </h1>
 
-          <div className="mb-8 text-center">
-            <p className="text-black">
-              Share your wishlist items with everyone with{" "}
-              <b className="underline">ShareWish</b>
-            </p>
-          </div>
-          <div className="mb-4 flex justify-center">
-            <Link href="/signin">
-              <div className="text-blue-700 font-bold opacity-60 hover:opacity-100 p-2 rounded-lg border-2 border-blue-700">
-                Sign in
-              </div>
-            </Link>
-            <span className="mx-2">|</span>
-            <Link href="/signup">
-              <div className="text-green-700 font-bold opacity-60 hover:opacity-100 p-2 rounded-lg border-2 border-green-700">
-                Sign up
-              </div>
-            </Link>
-          </div>
+        <div className="mb-8 text-center">
+          <p className="text-black">
+            Share your wishlist items with everyone with{" "}
+            <b className="underline">ShareWish</b>
+          </p>
+        </div>
+        <div className="mb-4 flex justify-center">
+          <Link href="/signin">
+            <div className="text-blue-700 font-bold opacity-60 hover:opacity-100 p-2 rounded-lg border-2 border-blue-700">
+              Sign in
+            </div>
+          </Link>
+          <span className="mx-2">|</span>
+          <Link href="/signup">
+            <div className="text-green-700 font-bold opacity-60 hover:opacity-100 p-2 rounded-lg border-2 border-green-700">
+              Sign up
+            </div>
+          </Link>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 };
 
