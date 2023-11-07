031dc6ad76e4f6cf78a67651fa15f81a8ea5288e
Priya <priyamvadha0112@gmail.com>
2023-11-07 16:21:15 +0530

    updated item/index.js

--- src/pages/api/item/index.js
+++ src/pages/api/item/index.js
@@ -32,17 +32,18 @@ const addItem = async (req, res) => {
     browser = await launchChromium({
       headless:true,
       args: [
-        // '--disable-gpu',
-        // '--disable-dev-shm-usage',
-        // '--disable-setuid-sandbox',
-        // '--no-first-run',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
         '--no-sandbox',
-        // '--no-zygote',
-        // '--deterministic-fetch',
-        // '--disable-features=IsolateOrigins',
-        // '--disable-site-isolation-trials',
-        // '--hide-scrollbars', 
-        // '--disable-web-security'
+        '--no-zygote',
+        '--deterministic-fetch',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',
+        '--hide-scrollbars', 
+        '--disable-web-security',
+        "--window-size=2000x1500"
     ],
     });
 
